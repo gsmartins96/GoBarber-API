4945fdc02b5e3527dfb142061677d23bdf9d0c93
Gabriel Martins <gsmartins96@hotmail.com>
2019-09-20 10:17:18 -0300

    :construction: começando a criar base de dados :rocket:

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    // criar o created-at
+    timestamps: true,
+    // padrão de tabelas e colunas atraves do underscored
+    // por exemplo: Ao inves de UserGroup, cria a tabela user_groups
+    underscored: true,
+    underscoredAll: true,
+  },
+};

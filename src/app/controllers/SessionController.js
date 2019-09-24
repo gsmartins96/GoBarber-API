84ec76f6f299f12d63cd59fd66da30d4a11f9faf
Gabriel Martins <gsmartins96@hotmail.com>
2019-09-24 16:25:07 -0300

    :arrow_up: adicionado Yup e validações
    foi adicionado o Yup para fazer as validações na aplicação

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,22 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
@@ -14,7 +26,7 @@ class SessionController {
 
     // utilza o await pois está usando o bcrypt
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: 'Password does not match' });
+      return res.status(401).json({ error: 'Password does not correct' });
     }
 
     const { id, nome } = user;

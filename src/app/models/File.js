b1d5c03d2a2d470fc1a6e6a13839c619f0e53bb4
Gabriel Martins <gsmartins96@hotmail.com>
2019-10-15 10:52:27 -0300

    :construction: random

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -9,6 +9,12 @@ class File extends Model {
         // Enviar as colunas do nosso BD atraves de um objeto
         name: Sequelize.STRING,
         path: Sequelize.STRING,
+        url: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return `http://localhost:3333/files/${this.path}`;
+          },
+        },
       },
       {
         sequelize,

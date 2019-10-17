c3ee05b6f636157ad520687b3aad2919b098b0bd
Gabriel Martins <gsmartins96@hotmail.com>
2019-10-17 11:00:12 -0300

    :construction: listar agendamentos de um usuario

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,35 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'nome'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -13,10 +40,10 @@ class AppointmentController {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const provider_id = req.body;
+    const { provider_id, date } = req.body;
 
     const isProvider = await User.findOne({
-      where: { id: provider_id, provide: true },
+      where: { id: provider_id, provider: true },
     });
 
     if (!isProvider) {
@@ -25,7 +52,32 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
-    return res.json();
+    const hourStart = startOfHour(parseISO(date));
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permited' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    return res.json(appointment);
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -19,11 +19,14 @@ routes.post('/sessions', SessionController.store);
 
 // Colocando esse middleware global, ele só vai para as de baixo daqui
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
 routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 export default routes;
